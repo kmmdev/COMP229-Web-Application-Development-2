9823e00b2cb8bf0b571967590481404ec7bd4c73
Manoj Kumar <kumar.9195m@gmail.com>
2025-09-26 19:16:19 -0400

    Optimize for Netlify deployment

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/COMP229-Web-Application-Development/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
